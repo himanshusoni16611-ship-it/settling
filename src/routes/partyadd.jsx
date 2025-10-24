18c980832a7b578fd12f5488423c8f8b4cd4f259
himanshusoni <himanshusoni16611@gmail.com>
2025-10-24 07:05:19 -0700

    Update fetch URLs to use Render server

--- src/routes/partyadd.jsx
+++ src/routes/partyadd.jsx
@@ -43,7 +43,7 @@ pnm,gnm,mob,jdate
 
   try{
 
-    const response = await fetch('setling-fbdsfz-apbkdznfo-settlings.vercel.app/partyadd',{
+    const response = await fetch('https://server-ufa1.onrender.com/partyadd',{
 method:'POST',
 headers:{
     'Content-Type':'application/json',
@@ -77,7 +77,7 @@ const alertdeelte = async (e, id) => {
 
   if (result.isConfirmed) {
     try {
-      const response = await fetch(`setling-fbdsfz-apbkdznfo-settlings.vercel.app/partyadd/${id}`, {
+      const response = await fetch(`https://server-ufa1.onrender.com/partyadd/${id}`, {
         method: 'DELETE',
         headers: { 'Content-Type': 'application/json' },
       });
