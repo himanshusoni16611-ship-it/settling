c1e50aae659978ad1136803084d8a14f0e74e2a0
himanshusoni <himanshusoni16611@gmail.com>
2025-10-24 06:58:02 -0700

    Change single quotes to double quotes

--- src/routes/deleteall.jsx
+++ src/routes/deleteall.jsx
@@ -14,7 +14,7 @@ const result = await Swal.fire({
 })
 if(!result.isConfirmed)return;
 try{
-const url = await fetch('https://server-ufa1.onrender.com/deleteall',{
+const url = await fetch("https://server-ufa1.onrender.com/deleteall",{
     method:'DELETE',
     headers:{
         'Content-Type':'application/json',
