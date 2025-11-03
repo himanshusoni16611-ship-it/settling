ee8f28b6c003bfe72ad4d134ca01df66d4204a5c
himanshusoni <himanshusoni16611@gmail.com>
2025-11-02 23:46:57 -0800

    Update API endpoint for fetching party list

--- src/Context/partycontext.js
+++ src/Context/partycontext.js
@@ -9,7 +9,7 @@ export const PartyProvider = ({ children }) => {
 
   const fetchPartyList = async () => {
     try {
-      const response = await fetch("https://server-ufa1.onrender.com/partyadd", {
+      const response = await fetch("http://178.16.139.134:5000/api/partyadd", {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
       });
