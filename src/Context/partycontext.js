de3e292be2644fcdacc9d2aa944339f0dee10cce
himanshusoni <himanshusoni16611@gmail.com>
2025-11-08 08:25:21 -0800

    Update partycontext.js

--- src/Context/partycontext.js
+++ src/Context/partycontext.js
@@ -14,7 +14,7 @@ export const PartyProvider = ({ children }) => {
     setError(null);
     try {
       // CORRECTED: Using the right domain www.setling.in
-      const response = await fetch("/api/partyadd", {
+      const response = await fetch("http://www.setling.in/partyadd", {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
       });
