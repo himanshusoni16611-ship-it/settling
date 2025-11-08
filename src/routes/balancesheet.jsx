718276eaad422346ec494e568a257a2d0b92b2f5
himanshusoni <himanshusoni16611@gmail.com>
2025-11-08 05:59:24 -0800

    Update balancesheet.jsx

--- src/routes/balancesheet.jsx
+++ src/routes/balancesheet.jsx
@@ -8,7 +8,7 @@ const Bs = () => {
 
   const getreq = async () => {
     try {
-      const response = await fetch('/balancesheet');
+      const response = await fetch('http://www.setling.in/balancesheet');
       if (!response.ok) throw new Error(`HTTP error ${response.status}`);
       const json = await response.json();
 
